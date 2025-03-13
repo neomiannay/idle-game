86c394261690f2c8c6188c61c7f7d6a12c46d1b1
Lucien <lucien.boisseau-sable@smice.com>
2025-03-13 17:35:28 +0100

    fix button & global.scss;

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,20 +1,18 @@
-import { memo } from 'react'
-
-import classNames from 'classnames'
-
-import styles from './Button.module.scss'
+import { ButtonHTMLAttributes, memo } from "react";
+import classNames from "classnames";
+import styles from "./Button.module.scss";
 
 type ButtonProps = {
-  className?: string
-  title: string
-}
+  className?: string;
+  title: string;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button = ({ className, title, ...props }: ButtonProps) => {
   return (
-    <button className={ classNames(styles.wrapper, className) } { ...props }>
-      { title }
+    <button className={classNames(styles.wrapper, className)} {...props}>
+      {title}
     </button>
-  )
-}
+  );
+};
 
-export default memo(Button)
+export default memo(Button);
