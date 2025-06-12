b5bfc8e02f226d905be3dce56ad7fcfe85d29cc3
neo_mny <miannayneo@gmail.com>
2025-06-12 05:11:03 +0200

    feat (EndScreen) : fix animation for top content ☝️

--- src/blocks/end-screen/EndScreen.tsx
+++ src/blocks/end-screen/EndScreen.tsx
@@ -1,30 +1,50 @@
-import React, { PropsWithChildren, useMemo } from 'react'
+import React, { PropsWithChildren } from 'react'
 
 import classNames from 'classnames'
-import styles from './EndScreen.module.scss'
 import { motion } from 'motion/react'
 import AdaptativeText from 'components/adaptative-text/AdaptativeText'
-import { baseVariants, fadeAppear } from 'core/animation'
+import { baseVariants, fadeAppear, stagger } from 'core/animation'
+
+import styles from './EndScreen.module.scss'
 
 type EndScreenProps = PropsWithChildren<{
-	className?: string
+  className?: string
 }>
 
 const EndScreen = ({ className, ...props }: EndScreenProps) => {
+  return (
+    <motion.div
+      className={ classNames(styles.wrapper, className) }
+      { ...props }
+      { ...baseVariants }
+      { ...stagger(0.3, 0.4) }
+    >
+      <div
+        className={ styles.titleContainer }
+      >
+        <motion.div
+          { ...fadeAppear() }
+          custom={{ invert: true }}
+        >
+          <AdaptativeText
+            className={ classNames(styles.title) }
+          >
+            Félicitations
+          </AdaptativeText>
+        </motion.div>
 
+        <motion.div
+          className={ classNames(styles.second) }
+          { ...fadeAppear() }
+          custom={{ invert: true }}
+        >
+          <AdaptativeText className={ styles.adpative }>vous avez généré 6 400 000 000,00 € en 3h 20m 10s</AdaptativeText>
+        </motion.div>
+      </div>
 
-	return (
-		<div className={classNames(className)} {...props}>
-			<motion.div { ...baseVariants }
-			 {...fadeAppear()}>
-				<AdaptativeText
-					className={classNames(styles.congrats)}
-				>
-					Félicitations
-				</AdaptativeText>
-			</motion.div>
-		</div>
-	)
+      <div className={ styles.content } />
+    </motion.div>
+  )
 }
 
 export default EndScreen
--- src/hooks/useGamePersistence.tsx
+++ src/hooks/useGamePersistence.tsx
@@ -16,7 +16,7 @@ export const getStorageKey = () => {
 export function useGamePersistence ({
   onLoad,
   onSave,
-  autoSaveInterval = 6000, // Save every minute by default
+  autoSaveInterval = 60000, // Save every minute by default
   storageKey = getStorageKey()
 }: PersistenceOptions) {
   const saveGameState = useCallback(() => {
