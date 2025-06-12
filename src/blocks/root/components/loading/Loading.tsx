f7f3844845566de9c3a6bd21055a5ff43f95553b
Lucien <lucien.boisseau-sable@smice.com>
2025-06-12 04:22:41 +0200

    fix loading: style & colors;

--- src/blocks/root/components/loading/Loading.tsx
+++ src/blocks/root/components/loading/Loading.tsx
@@ -7,7 +7,10 @@ import styles from './Loading.module.scss'
 function Loading () {
   return (
     <div className={ styles.loading }>
-      <GradientText>
+      <GradientText
+        startColor='var(--color-money)'
+        endColor='var(--fill-60)'
+      >
         Loading...
       </GradientText>
     </div>
